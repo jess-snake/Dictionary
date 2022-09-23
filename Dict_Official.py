aef10038944067bd037c056fc28d095edc5a1f3e
jess-snake <112585290+jess-snake@users.noreply.github.com>
2022-09-23 16:44:39 -0700

    Add files via upload
    Main Dictionary script and pickle script that goes with it.

---
+++ Dict_Official.py
@@ -0,0 +1,291 @@
+import pandas as pd, time, string, os, pickle, xlsxwriter
+import Dictionary
+import DictionPickle as dp
+
+
+#Pull in dictionary
+
+#Read the words from input
+def RecordWords():
+
+#Create variables
+    global words
+    global create_list
+    words = input()
+
+#Punctuation replaced by spacing
+    punct_1 = ['-','.',',','—',';',':','?','!','/','(',')']
+    punct_2 = ['"']
+    for p in punct_1:
+        words = words.replace(p,' ')
+    for p in punct_2:
+        words = words.replace(p,'')
+        
+#Use split to create a list from string
+    words = string.capwords(words)
+    create_list = words.split()
+    
+#Remove dupes from the list
+    word_list = [*set(create_list)]
+    
+#Add entries to the current dictionary
+#We will use the current dictionary to make a new dictionary
+    for w in word_list:
+        if len(curr_dict) != 0:
+            wl_last_entry = list(curr_dict)[-1]
+        else:
+            wl_last_entry = 0
+                
+        wl_new_entry = wl_last_entry + 1
+        curr_dict[wl_new_entry] = w
+    
+#New dictionary
+    
+#Variables
+    global new_dict
+    new_list = []
+    new_dict = {}
+    num = 0
+ 
+#Separate current dictionary values, remove dupes, and alphabetize in a new list 
+    curr_dict_val = curr_dict.values()
+    for v in curr_dict_val:
+        new_list.append(v)
+    new_list = [*set(new_list)]
+    new_list = sorted(new_list)
+    
+#Add the new list values to a new dictionary 
+    for i in new_list:
+        num = num + 1
+        new_dict[num] = i
+    #print(new_dict)
+    
+#Check both new and old dicts, prompt user if they would like to update the current dictionary
+    if new_dict != old_dict:
+        print('You have new words to be added to your dictionary. Would you like to add them now?')
+        AddToDict()
+    else:
+        print('You did not use any new words this time.')
+ 
+#Check if user wants to update the current dictionary 
+def AddToDict():
+    yes_no_in = input()
+    yes_no = yes_no_in.lower()
+    if yes_no == 'yes':
+
+#If yes, current dictionary is updated to be the new dictionary
+        curr_dict = new_dict
+        dp.save_diction(curr_dict)
+        print('Dictionary updated successfully.')
+        print(curr_dict)
+        time.sleep(1)
+        print('. . .')
+        time.sleep(2)
+        print('Would you like to export the current dictionary?')
+        exportCheck()
+        
+#If no, current dictionary is kept as old dictionary
+    elif yes_no == 'no':
+        curr_dict = old_dict
+        print('No updates were made.')
+        print(curr_dict)
+        time.sleep(1)
+        print('. . .')
+        time.sleep(2)
+        print('Would you like to export the current dictionary?')
+        exportCheck()
+    else:
+        print("Sorry, I didn't get that.")
+        AddTryAgain() 
+    
+def AddTryAgain():
+    time.sleep(2)
+    print("Would you like to add new words to your dictionary? (Type 'yes' or 'no')")
+    AddToDict()
+    
+#Check if user wants to export        
+def exportCheck():
+    yes_no_in = input()
+    yes_no = yes_no_in.lower()
+    if yes_no == 'yes':
+    
+    #Check if the file was created this session
+        if newFile == "True":
+            print("Exporting...")
+            time.sleep(1)
+            ExportDict()
+            
+    #If the file is existing, ask if the user wants to Replace or Save new
+        else:
+            print("Would you like to Replace your current file or Save a new version? (Type 'Replace' or 'Save new')")
+            SaveOrRep()
+    elif yes_no == 'no':
+        print('Dictionary was not exported.')
+    else:
+        print("Sorry, I didn't get that.")
+        ExTryAgain()
+        
+def ExTryAgain():
+    time.sleep(2)
+    print("Would you like to add new words to export your dictionary? (Type 'yes' or 'no')")
+    
+#Check user input for Replace or Save new
+    
+def SaveOrRep():
+    SavRep_in = input()
+    SavRep = SavRep_in.lower()
+    
+    if SavRep == 'replace':
+        print("File will be replaced using existing filename: " + fn.param)
+        time.sleep(2)
+        print("Exporting...")
+        time.sleep(1)
+        ExportDict()
+    elif SavRep == 'save new':
+        dp.UpdateFile("filename.pickle")
+        print("File will be saved as a new version: " + fn.param)
+        time.sleep(2)
+        print("Exporting...")
+        time.sleep(1)
+        ExportDict()
+    else:
+        print("I'm sorry, I didn't get that.")
+        time.sleep(1)
+        savTryAgain()
+    
+def savTryAgain():
+    print("Do you want to Replace your current file or save a new version? (Type 'Replace' or 'Save new')")
+    SaveOrRep()
+    
+def ExportDict():
+    
+#Set variables
+    obj = dp.load_object("filename.pickle")
+    filename = obj.param
+    data = new_dict
+    
+    data_val = list(data.values())
+
+    df = pd.DataFrame({"Number" : data.keys(),
+                        "Term" : data_val})
+                        
+    dictTable = (df.T)
+    
+    print(dictTable)
+    
+    header_names = ['Number','Term']
+#Create a Pandas Excel writer using XlsxWriter as the engine.
+    writer = pd.ExcelWriter(filename, engine='xlsxwriter')
+    
+#Convert the dataframe to an XlsxWriter Excel object. Note that we turn off
+#the default header and skip one row to allow us to insert a user defined
+#header.
+
+    df.to_excel(writer, sheet_name = 'Sheet1', startrow = 1, index = False, header = False)
+    
+#Get the xlsxwriter workbook and worksheet objects.
+
+    workbook  = writer.book
+    worksheet = writer.sheets['Sheet1']
+    
+#Add a header format
+    header_format = workbook.add_format({
+        'bold': True,
+        'text_wrap': True,
+        'valign': 'top',
+        'fg_color': '#D7E4BC',
+        'border': 1})
+        
+#Write the column headers with the defined format.
+
+    for col_num, value in enumerate(df.columns.values):
+        worksheet.write(0, col_num, value, header_format)
+    
+#Close the Pandas Excel writer and output the Excel file.
+    writer.save()
+    print("Excel saved successfully.")
+        
+#Ask user to enter in text
+def PromptUser():
+
+#Load the filename in case it was just created
+    fn = dp.load_object("filename.pickle")
+    
+#Prompt user to enter text
+    print("Please enter in some text.")
+    RecordWords()
+    
+def checkFilename():
+#Create variable to track filename 
+    global fn
+
+#Create a variable to detect if a new file is created this session   
+    global newFile
+    
+    if os.path.isfile("filename.pickle"):
+        newFile = "False"
+        
+#If the filename already exists, load it
+        fn = dp.load_object("filename.pickle")
+        print("You have an existing file: " + fn.param)
+        time.sleep(1)
+        print("Would you like to create a new file?")
+        makeNew()
+    else:
+        print("No existing file.")
+        newFile = "True"
+        dp.fileprompt()
+        time.sleep(2)
+        PromptUser()
+        
+def makeNew():
+    New_YN = input()
+    New_YN = New_YN.lower()
+    
+    if New_YN == 'yes':
+        dp.fileprompt()
+        
+#New file is created, update variable that is tracking it        
+        newFile = "True"
+        time.sleep(2)
+        PromptUser()
+        
+    elif New_YN == 'no':
+        print("Existing file will be used.")
+        time.sleep(1)
+        PromptUser()
+        
+    else:
+        print("Sorry, I didn't get that.")
+        time.sleep(2)
+        makeNewAgain()
+        
+def makeNewAgain():
+    print("Do you want to create a new file? (Type yes or no)")
+    makeNew()
+
+#Check for existing dictionary
+
+def dictCheck():
+    global old_dict
+    global curr_dict
+    
+    if os.path.isfile("dictionary.pickle"):
+        loadDict = dp.load_object("dictionary.pickle")
+        print("Loaded most recent dictionary...")
+        time.sleep(1)
+        
+        old_dict = loadDict
+        curr_dict = dict(loadDict)
+        
+    else:
+        old_dict = {}
+        curr_dict = {}
+        print("No dictionary found.")
+        time.sleep(1)
+        print("New dictionary created.")
+        time.sleep(1)
+    
+    checkFilename()
+
+dictCheck()
---
+++ DictionPickle.py
@@ -0,0 +1,120 @@
+import pickle, re 
+ 
+class SaveFile():
+    def __init__(self, param):
+        self.param = param
+        
+#Save the variable
+ 
+def save_object(obj):
+    try:
+        with open("filename.pickle", "wb") as f:
+            pickle.dump(obj, f, protocol=pickle.HIGHEST_PROTOCOL)
+    except Exception as ex:
+        print("Error during pickling object (Possibly unsupported):", ex)
+        
+    #Show the variable
+    
+    #obj = load_object("filename.pickle")
+    #print(obj.param)
+    
+    #Because obj is also a param, calling it here will show once,
+    #while the save_object and load_object functions technically process twice
+    #due to the save_object function creating the file and updating the file.
+    
+#Load the variable
+    
+def load_object(filename):
+    try:
+        with open(filename, "rb") as f:
+            return pickle.load(f)
+    except Exception as ex:
+        print("Error during unpickling object (Possibly unsupported):", ex)
+    
+#Ask user for variable input
+
+def fileprompt():
+    print("Please enter a name for your file.")
+    createFile()
+    
+def createFile():
+    global text
+    text = input()
+    filename = text + "_V1.xlsx"
+    
+#Use class and save_object function to store input
+    
+    obj = SaveFile(filename)
+    save_object(obj)
+    
+#Update file version number
+    
+def UpdateFile(filename):
+
+#Assumes file can be accessed so it's only called once
+    loadFile = pickle.load(open(filename, "rb"))
+    
+    if loadFile:
+        fn = loadFile.param
+        
+        verCheck = re.search('_V(.+?).xlsx', fn)
+        
+        if verCheck:
+        #Break down original filename to isolate num.xlsx 
+            breakdown_1 = fn.partition('_V')
+            grabIt_1 = breakdown_1[2]
+            grabIt_1 = str(grabIt_1)
+            
+        #Break down to isolate the num only
+            breakdown_2 = grabIt_1.partition('.xlsx')
+            grabIt_2 = breakdown_2[0]
+            grabIt_2 = int(grabIt_2)
+        
+        #Reassign version number
+            versionNum = grabIt_2 + 1
+            versionNum = str(versionNum)
+            
+        #Grab 1st portion of filename
+            FN_part1 = breakdown_1[0]
+            FN_part1 = str(FN_part1)
+            
+        #Create new filename
+            fnNew = FN_part1 + "_V" + versionNum + ".xlsx"
+            
+        else:
+        
+            print("File version will be saved as 'V1'.")
+
+            versionNum = 1
+
+            versionNum = str(versionNum)
+            
+            #Break down original filename to 1st portion
+            breakdown_1 = askForFile.partition('.xlsx')
+            FN_part1 = breakdown_1[0]
+            FN_part1 = str(FN_part1)
+
+            #Create new filename
+            fnNew = FN_part1 + "_V" + versionNum + ".xlsx"
+        
+            #Save file   
+            workbook.save(filename = fnNew)
+            print("Saved successfully as " + fnNew)
+            
+        obj = SaveFile(fnNew)
+        save_object(obj)
+        
+    else:
+        print("Couldn't load file.")
+        
+#Save the dictionary
+ 
+def save_diction(obj):
+    try:
+        with open("dictionary.pickle", "wb") as f:
+            pickle.dump(obj, f, protocol=pickle.HIGHEST_PROTOCOL)
+    except Exception as ex:
+        print("Error during pickling object (Possibly unsupported):", ex)
+     
+    #obj = load_object("dictionary.pickle")
+    #print(obj)
